df89000321a09bdd44b4d6fecf6593f8c54ce91c
Fizzor <36672655+Fizzor96@users.noreply.github.com>
2020-05-13 02:04:05 +0200

    Admin user added to homescreen
    Admin felhasználó a kezdőoldalhoz hozzáadva

--- protected/user/home.php
+++ protected/user/home.php
@@ -1,6 +1,7 @@
 <div class="body content">
     <div class="welcome">
         <h1>Hello</h1>
+        <h3>Belépés: felhasználó: norbi jelszó: norbika</h3><br>
         <p>Webprogramozás 2 beadandó feladat</p>
         <p>A project tartalma: meglepetés :P <br>
             Ha nem szereted a meglepetéseket, akkor nézd meg a forráskódot és rá fogsz jönni, ha kicsit is értesz a html és php nyelvekhez.
